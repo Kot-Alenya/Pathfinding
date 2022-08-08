cf05d1d509b79014e0a8d3effca5fbf8707aa61e
Kot-Alenya <nevskial686@gmail.com>
2022-08-08 17:14:47 +0300

    Node.cs
    Structure indicating the sector

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Navigation
+{
+    public struct Node : IEquatable<Node>
+    {
+        public Sector Sector;
+        public Point Position;
+
+        public Node(Sector sector, Point position)
+        {
+            Sector = sector;
+            Position = position;
+        }
+
+        public bool Equals(Node node) => node.Sector == Sector && node.Position.Equals(Position);
+    }
+}
